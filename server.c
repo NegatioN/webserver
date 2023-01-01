8a517e1af126d5095cb934764bb9d957020c947e
NegatioN <joakimrishaug@gmail.com>
2023-01-01 14:42:40 +0100

    Mostly copy code that looks really similar to what i had, and now it accepts a connection :/

--- server.c
+++ server.c
@@ -1,5 +1,16 @@
-#include<stdio.h>
-#include<sys/socket.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 //https://news.ycombinator.com/item?id=34037547
 // relevant man pages:
@@ -15,29 +26,127 @@ Back in school, our teacher had us write a simple forking server and consult the
 
 So, for "resource" I recommend the HTTP example on Wikipedia, as well as "Beej's guide to network programming" (as have others), as well as the man pages for quick reference.
  */
+#define PORT "3490"  // the port users will be connecting to
+#define BACKLOG 10   // how many pending connections queue will hold
+
+
+void sigchld_handler(int s)
+{
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int main() {
-    struct addrinfo hints;
-    struct addrinfo *result, *rp;
-    int sockfd, s;
+   int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+   struct addrinfo hints, *servinfo, *p;
+   struct sockaddr_storage their_addr; // connector's address information
+   socklen_t sin_size;
+   struct sigaction sa;
+   int yes=1;
+   char s[INET6_ADDRSTRLEN];
+   int rv;
 
-    // this is copied from man 3 getaddrinfo
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE;
-    hints.ai_protocol = 0;
+   // START connection options
+   memset(&hints, 0, sizeof hints);
+   hints.ai_family = AF_UNSPEC; // Dont care if IPv4 or IPv6
+   hints.ai_socktype = SOCK_STREAM; // SOCK_STREAM for TCP, SOCK_DGRAM for UDP
+   hints.ai_flags = AI_PASSIVE; // use my IP
 
-    s = getaddrinfo(NULL, 8080, &hints, &result);
-    printf("%d", s);
+   sa.sa_handler = sigchld_handler; // reap all dead processes
+   sigemptyset(&sa.sa_mask);
+   sa.sa_flags = SA_RESTART;
+   // END connection options
+
+   if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+       fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+       return 1;
+   }
+
+   // loop through all the results and bind to the first we can
+   for(p = servinfo; p != NULL; p = p->ai_next) {
+       if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+           perror("server: socket");
+           continue;
+       }
+
+       if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+           perror("setsockopt");
+           exit(1);
+       }
+
+       if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+           close(sockfd);
+           perror("server: bind");
+           continue;
+       }
+
+       break;
+   }
+
+   freeaddrinfo(servinfo); // all done with this structure
+
+   if (p == NULL)  {
+       fprintf(stderr, "server: failed to bind\n");
+       exit(1);
+   }
+
+   if (listen(sockfd, BACKLOG) == -1) {
+       perror("listen");
+       exit(1);
+   }
+
+   if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+       perror("sigaction");
+       exit(1);
+   }
+
+   printf("server: waiting for connections...\n");
+
+   while(1) {  // main accept() loop
+       sin_size = sizeof their_addr;
+       new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+       if (new_fd == -1) {
+           perror("accept");
+           continue;
+       }
+
+       inet_ntop(their_addr.ss_family,
+           get_in_addr((struct sockaddr *)&their_addr),
+           s, sizeof s);
+       printf("server: got connection from %s\n", s);
+
+       if (!fork()) { // this is the child process
+           close(sockfd); // child doesn't need the listener
+           if (send(new_fd, "Hello, world!", 13, 0) == -1)
+               perror("send");
+           close(new_fd);
+           exit(0);
+       }
+       close(new_fd);  // parent doesn't need this
+   }
+
+   return 0;
 
 
-    int sockfd = socket(AF_UNSPEC, SOCK_DGRAM, 0); // man 2 socket says there is usually only one viable protocol, so inputting 0 should be fine (?)
+    //sockfd = socket(AF_UNSPEC, SOCK_DGRAM, 0); // man 2 socket says there is usually only one viable protocol, so inputting 0 should be fine (?)
 
 
     //TODO see when someone has written something to the socket
     //TODO do TCP handshakes?
     //TODO write a http response into the socket?
-    printf("%d\n", sockfd);
-    return 0;
 }
